5a8cd5abda7fa8af7050b85701a19d5e20e44c0b
Abdel Oufkir <43834323+AbdelOufkir1@users.noreply.github.com>
2019-04-27 14:58:56 -0400

    add a column for the name of the expenses (#9)
    * add a column for the name of the expenses
    
    * added the cascade attribute to the Ref columns on the seed

--- services/config.js
+++ services/config.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES
-const dbName = 'financialdb';
+const dbName = "financialdb";
 const dbAddr = `postgres://localhost/${dbName}`;
 
 module.exports = {
-    dbAddr,
-}
+  dbAddr
+};
