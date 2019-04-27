705a1eaa1fde05671c3823b242ac29ed9e98a6da
Jorge Billini <jorgebillini@Pursuits-MacBook-Air.local>
2019-04-27 15:11:27 -0400

    added Expenses services

--- routes/expense.js
+++ routes/expense.js
@@ -3,15 +3,21 @@ const express = require('express');
 
 // ROUTE FUNCTIONS
 const createExpense = (request, response) => {
-    const {fixed, amount, user_id, statement_id,} = request.body;
+    const {fixed, amount, user_id, statement_id, name,} = request.body;
 }
 
 const getExpense = (request, response) => {
     const {id,} = request.params;
 }
+const getAllExpenses = async (request,response) => {
+    // let fixed = true;
+    const {user_id, statement_id,} = request.params;
+    // const fixedExpenses = await
+    
+}
 
 const updateExpense = (request, response) => {
-    const {fixed, amount, user_id, statement_id,} = request.body;
+    const {fixed, amount, name, id,} = request.body;
 }
 
 const deleteExpense = (request, response) => {
